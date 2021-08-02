20814ad54b2153d29f96a16af669580cd92f19c7
lucas-verissimo <verissimo.lucas1994@gmail.com>
2021-08-02 18:03:48 -0300

    Update Imagem + Contato

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-let height_header = document.getElementsByTagName("header").offsetHeight;
+var height_screen = ((window.innerHeight)-60).toString();
+height_screen += "px";
+var home123 = document.querySelector("#home");
+home123.style.height = height_screen
+
+var width_screen = window.innerWidth
+if (width_screen < 600) {
+    document.querySelector('#minha-foto').style.width = '100%';
+    document.querySelector('#minha-foto').style.margin = 'auto';
+}
+
+function clicou() {
+    window.open("https://api.whatsapp.com/send?phone=5531984784190", '_blank');
+}
